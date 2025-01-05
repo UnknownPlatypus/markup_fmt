47ee6d9f955818b723800bad3b670550ac48e0da
Thibaut Decombe <thibaut.decombe@gmail.com>
2025-01-05 12:29:05 +0100

    Normalize `rel` as a space separated list too
    Normalize `autocomplete` as a space separated list too
    
    
    
    Normalize `sandbox` as a space separated list too
    
    Normalize `accept-charset` as a space separated list too
    
    Normalize some`aria-*` as a space separated list
    
    
    
    Add an helper for attribute that should be space separated

--- markup_fmt/src/helpers.rs
+++ markup_fmt/src/helpers.rs
@@ -1,3 +1,4 @@
+use crate::state::State;
 use crate::Language;
 use aho_corasick::AhoCorasick;
 use std::sync::LazyLock;
@@ -164,3 +165,44 @@ pub(crate) fn parse_vento_tag(tag: &str) -> (&str, &str) {
 
 pub(crate) static UNESCAPING_AC: LazyLock<AhoCorasick> =
     LazyLock::new(|| AhoCorasick::new(["&quot;", "&#x22;", "&#x27;"]).unwrap());
+
+/// Checks if the given attribute name content should be space-separated.
+///
+/// These were found using the HTML attribute list, cross-referencing the HTML spec:
+/// - <https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes>
+/// - <https://html.spec.whatwg.org/multipage/>
+pub(crate) fn should_be_space_separated(name: &str, state: &State) -> bool {
+    name.eq_ignore_ascii_case("class")
+        || name.eq_ignore_ascii_case("aria-labelledby")
+        || name.eq_ignore_ascii_case("aria-describedby")
+        || name.eq_ignore_ascii_case("aria-controls")
+        || name.eq_ignore_ascii_case("aria-owns")
+        || name.eq_ignore_ascii_case("rel")
+            && state
+                .current_tag_name
+                .map(|name| {
+                    ["form", "a", "area", "link"]
+                        .iter()
+                        .any(|tag| tag.eq_ignore_ascii_case(name))
+                })
+                .unwrap_or_default()
+        || name.eq_ignore_ascii_case("autocomplete")
+            && state
+                .current_tag_name
+                .map(|name| {
+                    ["form", "input", "select", "textarea"]
+                        .iter()
+                        .any(|tag| tag.eq_ignore_ascii_case(name))
+                })
+                .unwrap_or_default()
+        || name.eq_ignore_ascii_case("sandbox")
+            && state
+                .current_tag_name
+                .map(|name| name.eq_ignore_ascii_case("iframe"))
+                .unwrap_or_default()
+        || name.eq_ignore_ascii_case("accept-charset")
+            && state
+                .current_tag_name
+                .map(|name| name.eq_ignore_ascii_case("form"))
+                .unwrap_or_default()
+}
